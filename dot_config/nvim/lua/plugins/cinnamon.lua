e388ff1432484c5f0b5304febfba250ad5ba6c45
Quincy Jo <me@quincyjo.com>
2025-12-21 14:09:55 -0500

    Add smear-cursor and cinnamon.

---
+++ dot_config/nvim/lua/plugins/cinnamon.lua
@@ -0,0 +1,63 @@
+return {
+    'declancm/cinnamon.nvim',
+    version = '*',
+    event = 'VeryLazy',
+    ---@module 'cinnamon'
+    ---@type CinnamonOptions
+    opts = {
+        keymaps = {
+            basic = false,
+            extra = false,
+        },
+        options = {
+            mode = 'window',
+            delay = 3,
+            max_delta = {
+                time = 100,
+            },
+            step_size = {
+                vertical = 1,
+            },
+        },
+    },
+    keys = function()
+        local cinnamon = require('cinnamon')
+        local keymaps = {}
+        local centered_commands = {
+            -- For some reason this makes it jump to nonexistent results.
+            'n', 'N',
+            '<C-o>', '<C-i>',
+            '<C-d>', '<C-u>',
+            'G', 'gg',
+            '}', '{',
+            ')', '(',
+            '*', '#',
+        }
+        local commands = {
+            'zt', 'zb', 'zz',
+            'z.', 'z<CR>', 'z-', 'z^', 'z+',
+            'zH', 'zL', 'zs', 'ze', 'zh', 'zl',
+            'j', 'k', 'h', 'l',
+            '<C-e>', '<C-y>',
+            '<C-f>', '<C-b>',
+            '0', '^', '$',
+            'w', 'W', 'b', 'B',
+        }
+
+        for _, cmd in ipairs(centered_commands) do
+            table.insert(keymaps, {
+                cmd,
+                function() cinnamon.scroll(cmd .. 'zz') end,
+                mode = { 'n', 'v' }
+            })
+        end
+        for _, cmd in ipairs(commands) do
+            table.insert(keymaps, {
+                cmd,
+                function() cinnamon.scroll(cmd) end,
+                mode = { 'n', 'v' }
+            })
+        end
+        return keymaps
+    end,
+}
---
+++ dot_config/nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,14 @@
+return {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+        cursor_color = "#f83d19",
+        stiffness = 0.8,
+        trailing_stiffness = 0.6,
+        distance_stop_animating = 0.5,
+        legacy_computing_symbols_support = true,
+
+        smear_insert_mode = true,
+        legacy_computing_symbols_support_vertical_bars = true,
+        distance_stop_animating_vertical_bar = 0.1,
+    },
+}
