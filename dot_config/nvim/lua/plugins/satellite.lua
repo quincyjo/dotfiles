3c01e5f0359e0fd61bb97ef9866aeef55c2c5c27
Quincy Jo <me@quincyjo.com>
2026-01-23 00:29:42 -0500

    Nvim: Use overlap for satellite gitsigns.

--- dot_config/nvim/lua/plugins/satellite.lua
+++ dot_config/nvim/lua/plugins/satellite.lua
@@ -1,9 +1,20 @@
 return {
     'lewis6991/satellite.nvim',
     opts = {
+        width = 20,
         handlers = {
             cursor = {
                 enable = false,
+            },
+            gitsigns = {
+                enable = true,
+                -- Workaround for https://github.com/lewis6991/satellite.nvim/issues/94
+                overlap = true,
+                signs = {
+                    add = "│",
+                    change = "│",
+                    delete = "-",
+                },
             },
         },
     },
