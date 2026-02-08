35a8f0b19286fee1634b1c2ed4775eea42372fc6
Quincy Jo <me@quincyjo.com>
2026-02-07 21:34:05 -0500

    Nvim: Add visual mode to clipboard keymaps.

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -27,10 +27,10 @@ vim.keymap.set('n', '<C-w>v', '<cmd>vsplit<cr>', { desc = 'Vertical Split' })
 vim.keymap.set('n', '<C-w>s', '<cmd>split<cr>', { desc = 'Horizontal Split' })
 
 -- Clipboard
-vim.keymap.set('n', '<leader>y', '"+y', { desc = 'Yank to system clipboard' })
-vim.keymap.set('n', '<leader>Y', '"+Y', { desc = 'Yank to system clipboard' })
-vim.keymap.set('n', '<leader>p', '"+p', { desc = 'Paste from system clipboard' })
-vim.keymap.set('n', '<leader>P', '"+P', { desc = 'Paste from system clipboard' })
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { desc = 'Yank to system clipboard' })
+vim.keymap.set({ 'n', 'v' }, '<leader>Y', '"+Y', { desc = 'Yank to system clipboard' })
+vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p', { desc = 'Paste from system clipboard' })
+vim.keymap.set({ 'n', 'v' }, '<leader>P', '"+P', { desc = 'Paste from system clipboard' })
 
 -- Window navigation
 vim.keymap.set("n", "<C-h>", "<C-w>h")
