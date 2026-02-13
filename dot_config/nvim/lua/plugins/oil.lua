842f87990761a09a6eca326e2372a2ab6c077faa
Quincy Jo <me@quincyjo.com>
2026-02-12 20:39:17 -0500

    Nvim: Switch to oil-git-status over oil-git.

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -1,17 +1,17 @@
 return {
-    {
-        'stevearc/oil.nvim',
-        ---@module 'oil'
-        ---@type oil.SetupOpts
-        opts = {},
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        lazy = false,
-        keys = {
-            { mode = { "n" }, "-", "<cmd>Oil<cr>" }
-        }
+    "refractalize/oil-git-status.nvim",
+    dependencies = {
+        {
+            'stevearc/oil.nvim',
+            ---@module 'oil'
+            ---@type oil.SetupOpts
+            opts = {},
+            dependencies = { "nvim-tree/nvim-web-devicons" },
+            lazy = false,
+            keys = {
+                { mode = { "n" }, "-", "<cmd>Oil<cr>" }
+            }
+        },
     },
-    {
-        "benomahony/oil-git.nvim",
-        dependencies = { "stevearc/oil.nvim" },
-    }
+    config = true,
 }
